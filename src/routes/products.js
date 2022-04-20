610aded5f72f6e1a4a730fd38b201ded1d44b54b
Pablo Valles <valles.pablo@hotmail.com>
2022-04-20 00:03:11 -0300

    falta finalizar subida de imagenes

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,31 @@
 // ************ Require's ************
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const router = express.Router();
 
 // ************ Controller Require ************
 const productsController = require("../controllers/productsController");
+
+// Multer config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/images"));
+  },
+  filename: (req, file, cb) => {
+    let archivo = Date.now() + " - " + path.extname(file.originalname);
+    cb(null, archivo);
+  },
+});
+
+const upload = multer({ storage });
 
 /*** GET ALL PRODUCTS ***/
 router.get("/", productsController.index);
 
 /*** CREATE ONE PRODUCT ***/
 router.get("/create", productsController.create);
-router.post("/create", productsController.store);
+router.post("/create", upload.any(), productsController.store);
 
 /*** GET ONE PRODUCT ***/
 router.get("/detail/:id", productsController.detail);
